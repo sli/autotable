b4b416099e82e10b551ccb966b231d46d2789490
sli <cody.maden@stratuscube.com>
2019-12-11 22:59:23 -0500

    drag and drop is now functional :sparkles:

--- src/Autotable.elm
+++ src/Autotable.elm
@@ -1,9 +1,9 @@
 module Autotable exposing (..)
 
-import Browser
 import Html exposing (Html, a, div, span, table, tbody, td, text, th, thead, tr)
 import Html.Attributes exposing (class, style)
-import Html.Events exposing (onClick)
+import Html.Events exposing (on, onClick)
+import Json.Decode as D
 import PageCss exposing (pageCss)
 import Tuple exposing (first, second)
 
@@ -28,11 +28,36 @@ type alias Model a =
     { columns : List (Column a)
     , data : List a
     , sorting : List Sorting
+    , dragging : Maybe String
     }
 
 
 type Msg
     = Sort String
+    | DragStart String
+    | DragEnd
+    | DragOver String
+    | Drop
+
+
+onDragStart : msg -> Html.Attribute msg
+onDragStart msg =
+    on "dragstart" <| D.succeed msg
+
+
+onDragEnd : msg -> Html.Attribute msg
+onDragEnd msg =
+    on "dragend" <| D.succeed msg
+
+
+onDragOver : msg -> Html.Attribute msg
+onDragOver msg =
+    on "dragover" <| D.succeed msg
+
+
+onDrop : msg -> Html.Attribute msg
+onDrop msg =
+    on "drop" <| D.succeed msg
 
 
 swapDirection : Sorting -> Sorting
@@ -55,6 +80,16 @@ findColumn columns label =
     List.head <| List.filter (\c -> c.label == label) columns
 
 
+indexForColumn : String -> List (Column a) -> Maybe Int
+indexForColumn label columns =
+    case List.head <| List.filter (\( i, c ) -> c.label == label) <| List.indexedMap (\i c -> ( i, c )) columns of
+        Just ( i, _ ) ->
+            Just i
+
+        Nothing ->
+            Nothing
+
+
 setOrder : Direction -> List a -> List a
 setOrder direction data =
     case direction of
@@ -67,7 +102,7 @@ setOrder direction data =
 
 init : List (Column a) -> List a -> Model a
 init columns data =
-    { columns = columns, data = data, sorting = [] }
+    { dragging = Nothing, columns = columns, data = data, sorting = [] }
 
 
 update : Msg -> Model a -> Model a
@@ -105,6 +140,45 @@ update msg model =
             in
             { model | sorting = sorting, data = data }
 
+        DragStart target ->
+            { model | dragging = Just target }
+
+        DragEnd ->
+            { model | dragging = Nothing }
+
+        DragOver target ->
+            -- I hate it, but it works for now.
+            case indexForColumn target model.columns of
+                Just targetPosition ->
+                    case model.dragging of
+                        Just label ->
+                            if label /= target then
+                                case List.head <| List.filter (\c -> c.label == label) model.columns of
+                                    Just column ->
+                                        let
+                                            cleaned =
+                                                List.filter (\c -> c.label /= label) model.columns
+
+                                            columns =
+                                                List.take targetPosition cleaned ++ [ column ] ++ List.drop targetPosition cleaned
+                                        in
+                                        { model | columns = columns }
+
+                                    Nothing ->
+                                        model
+
+                            else
+                                model
+
+                        Nothing ->
+                            model
+
+                Nothing ->
+                    model
+
+        Drop ->
+            { model | dragging = Nothing }
+
 
 view : Model a -> (Msg -> msg) -> Html msg
 view model toMsg =
@@ -126,7 +200,14 @@ view model toMsg =
                                 Nothing ->
                                     " "
                     in
-                    th [ onClick <| toMsg <| Sort c.label ]
+                    th
+                        [ onClick <| toMsg <| Sort c.label
+                        , onDragStart <| toMsg <| DragStart c.label
+                        , onDragEnd <| toMsg DragEnd
+                        , onDragOver <| toMsg <| DragOver c.label
+                        , Html.Attributes.draggable "true"
+                        , style "user-select" "none"
+                        ]
                         [ text <| c.label
                         , span [ style "margin-left" "10px", style "font-size" "10pt" ] [ text sorting ]
                         ]
